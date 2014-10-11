6b25e3bac0c43b53b7b1790dfe3a6bd0f692b9da
m3tr0n0m3 <yanakiev_g@outlook.com>
2014-10-11 16:19:07 +0300

    added sample listview entries for test

--- HotelCatalog/src/com/transformers/hotelcatalog/MainActivity.java
+++ HotelCatalog/src/com/transformers/hotelcatalog/MainActivity.java
@@ -1,21 +1,39 @@
 package com.transformers.hotelcatalog;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends Activity {
 
+	private ListView listview;
+	private List<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateListView();
     }
 
 
-    @Override
+    private void populateListView() {
+		// TODO Auto-generated method stub
+		String[] items = {"Hotel1", "Hotel2", "Hotel3", "Hotel4"}; 
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.hotel_view, items); 
+		
+		this.listview = (ListView)this.findViewById(R.id.listViewHotels);
+		listview.setAdapter(adapter);
+	} 
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
