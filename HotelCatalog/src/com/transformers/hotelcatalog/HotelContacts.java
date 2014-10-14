22c07a64cffbd097ae3a3808e4e197746c768a04
m3tr0n0m3 <yanakiev_g@outlook.com>
2014-10-15 01:43:17 +0300

    Basic google maps functionality added

--- HotelCatalog/src/com/transformers/hotelcatalog/HotelContacts.java
+++ HotelCatalog/src/com/transformers/hotelcatalog/HotelContacts.java
@@ -1,21 +1,29 @@
 package com.transformers.hotelcatalog;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class HotelContacts extends Activity {
 
-	@Override
+	private final LatLng TELERIK_LOCATION = new LatLng(42.65084700000001, 23.379431000000068);
+	private GoogleMap map;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hotel_contacts);
 
-		ContactsFragment frag = new ContactsFragment();
-		FragmentManager manager = getFragmentManager();
-		FragmentTransaction transaction = manager.beginTransaction();
-		transaction.add(R.id.contact_frag, frag, "Contacts Fragment");
-		transaction.commit();
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		map.addMarker(new MarkerOptions().position(TELERIK_LOCATION).title("Telerik Academy"));
+		map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+		
+		CameraUpdate update = CameraUpdateFactory.newLatLngZoom(TELERIK_LOCATION, 17);
+		map.animateCamera(update);
 	}
 }
