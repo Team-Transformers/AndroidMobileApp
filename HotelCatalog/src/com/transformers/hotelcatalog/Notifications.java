8bc204b17cf4ef8d3f0476698cc1fb25b17b26e5
m3tr0n0m3 <yanakiev_g@outlook.com>
2014-10-16 23:30:37 +0300

    Added notification for no connection

---
+++ HotelCatalog/src/com/transformers/hotelcatalog/Notifications.java
@@ -0,0 +1,13 @@
+package com.transformers.hotelcatalog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Notifications extends Activity {
+   
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+}
